a7f5e75c8ac1c73c299444f8b3242031b77430a8
maycon-mello <maycon.mello@objectedge.com>
2016-12-02 15:52:15 -0200

    Add support to node 4

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-const { prepareAndRun } = require('./lib/run');
-
-
-module.exports = {
-  serve(config) {
-    return prepareAndRun(config, 'serve');
-  },
-
-  build(config) {
-    return prepareAndRun(config, 'build');
-  },
-
-  test(config) {
-    return prepareAndRun(config, 'test');
-  },
-}
+module.exports = require('./build/index.js');
